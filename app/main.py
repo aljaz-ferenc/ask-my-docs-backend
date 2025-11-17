cbc96430e800efc5e5d100aaa9245395faf5d71a
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2025-11-18 00:54:27 +0100

    feat: stream llm response via SSE (#2)

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[origin],
     allow_credentials=True,
-    allow_methods=["GET", 'POST'],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
